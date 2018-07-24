baa2b0da85b813a4f252723dc09fc328dc23f2a8
KowsalyaK <kowsalyakowsi78@gmail.com>
2018-07-24 12:26:10 +0530

    Create RomanToInteger.py

---
+++ PlayerLevel/RomanToInteger.py
@@ -0,0 +1,21 @@
+r=raw_input()
+if(r=='I'):
+     print 1
+if(r=='II'):
+     print 2
+if(r=='III'):
+    print 3
+if(r=='IV'):
+    print 4
+if(r=='V'):
+     print 5
+if(r=='VI'):
+     print 6
+if(r=='VII'):
+     print 7
+if(r=='VIII'):
+     print 8
+if(r=='IX'):
+     print 9
+if(r=='X'):
+    print 10
