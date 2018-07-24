146d15bf3c18a5e7af586300a7341ea9c9c64729
KowsalyaK <kowsalyakowsi78@gmail.com>
2018-07-24 12:03:45 +0530

    Update NumberToWords.py

--- Beginner level/NumberToWords.py
+++ Beginner level/NumberToWords.py
@@ -1,5 +1,5 @@
 number2words= {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five',
             6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine'}
 number=input()
-if(number>1 and number<9):
+if(number>=1 and number<=10):
     print(number2words[number])
